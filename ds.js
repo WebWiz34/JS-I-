af95c885f664b53d295b86d72b38886c92348739
WebWiz34 <100660756+WebWiz34@users.noreply.github.com>
2022-04-14 11:26:43 -0700

    Create ds.js

---
+++ ds.js
@@ -0,0 +1,25 @@
+alert("Hello there what is your name");
+
+alert("Lets get to know each other more");
+
+var questionOne = prompt("What is your name?");
+
+document.getElementById("blankOne").innerHTML = questionOne;
+
+var questionTwo = prompt("whats your favorite food");
+
+document.getElementById("blankTow").innerHTML = questionTwo;
+
+var questionThree = prompt("what is the name of your friend");
+
+document.getElementById("blankThree").innerHTML = questionThree;
+
+consol.log("Nice to meet you");
+
+var myname = "Kirubel";
+consol.log(myname);
+
+var theMonth = "January";
+var theYear = 2006;
+
+console.log(theMonth + theYear);
